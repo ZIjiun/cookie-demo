e41a758a53f03265346b934f155084f1269912af
Zijun <s410126080@gmail.com>
2023-04-09 19:52:20 +0800

    演示 server 的 cookie 傳送及接收

---
+++ src/main/java/com/Kent/web/AServlet.java
@@ -0,0 +1,28 @@
+package com.Kent.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/aServlet")
+public class AServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 發送 cookie
+
+        // 1. 創建 cookie 物件
+        Cookie cookie = new Cookie("username", "zs");
+
+        // 2. 發送 cookie. response
+        resp.addCookie(cookie);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
---
+++ src/main/java/com/Kent/web/BServlet.java
@@ -0,0 +1,34 @@
+package com.Kent.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/bServlet")
+public class BServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 取得 cookie
+
+        // 1. 取得 cookie 陣列
+        Cookie[] cookies = req.getCookies();
+
+        // 2. 取得對應到的 cookie
+        for (Cookie cookie: cookies) {
+            String name = cookie.getName();
+            if("username".equals(name)) {
+                String value = cookie.getValue();
+                System.out.println(name+":"+value);
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
